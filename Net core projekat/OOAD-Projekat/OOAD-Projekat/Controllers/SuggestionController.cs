1be857e149198f8ab7bddbf43ace1388a349857e
Admir-Walker <aihtijarev2@etf.unsa.ba>
2021-05-29 18:02:45 +0200

    Prijedlog na search tokom kucanja.

---
+++ Net core projekat/OOAD-Projekat/OOAD-Projekat/Controllers/SuggestionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OOAD_Projekat.Data.Questions;
+using OOAD_Projekat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOAD_Projekat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuggestionController : ControllerBase
+    {
+        private readonly IQuestionsRepository questionsRepository;
+        public SuggestionController(IQuestionsRepository questionsRepository)
+        {
+            this.questionsRepository = questionsRepository;
+        }
+        [HttpGet("SearchInputSuggestion")]
+        public async Task<List<Question>> SuggestBasedOnSearchInput([FromQuery(Name = "searchParam")] string searchParam)
+        {
+            return await questionsRepository.Find(searchParam);
+        }
+    }
+}
