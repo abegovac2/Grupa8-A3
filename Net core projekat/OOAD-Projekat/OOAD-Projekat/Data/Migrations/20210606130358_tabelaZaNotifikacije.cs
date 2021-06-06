d2863ba896ab670fab1cd4ff13aa207995c8c459
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2021-06-06 15:09:06 +0200

    Dodana migracija za notifikacije

---
+++ Net core projekat/OOAD-Projekat/OOAD-Projekat/Data/Migrations/20210606130358_tabelaZaNotifikacije.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OOAD_Projekat.Data.Migrations
+{
+    public partial class tabelaZaNotifikacije : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
